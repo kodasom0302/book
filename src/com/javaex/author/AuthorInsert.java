0eaa8a2f52d3af205c24009b363a2d4257085ee6
dasom <dasom589@gmail.com>
2024-01-29 12:26:53 +0900

    2024-01-29 3교시

---
+++ src/com/javaex/author/AuthorInsert.java
@@ -0,0 +1,54 @@
+package com.javaex.author;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AuthorInsert {
+
+	public static void main(String[] args) {
+		
+		//번호 알아서, 이름 : 고다솜, 설명 : 막내반장
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+		// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/book_db";
+			conn = DriverManager.getConnection(url, "book", "book");
+			/*
+			String id="book";
+			String pw="book";
+			conn = DriverManager.getConnection(url, id, pw);
+			*/
+		// 3. SQL문 준비 / 바인딩 / 실행
+		// 4.결과처리
+		} catch (ClassNotFoundException e) {
+		System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		} finally {
+		// 5. 자원정리
+		try {
+		if (rs != null) {
+		rs.close();
+		}
+		if (pstmt != null) {
+		pstmt.close();
+		}
+		if (conn != null) {
+		conn.close();
+		}
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		}
+		}
+
+	}
+
+}
